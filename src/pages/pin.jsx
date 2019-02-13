fc79db30446ddef18ce11498b9d614e30b117eed
Chong Thao <tsoogthoj@hotmail.com>
2019-02-12 19:20:22 -0800

    fixed updating state when pressing numpad

--- src/pages/pin.jsx
+++ src/pages/pin.jsx
@@ -5,26 +5,32 @@ import Numpad from "../components/Numpad/numpad"
 import "./pin.css";
 
 class Pin extends Component {
+    state = {
+        passcode: []
+    };
 
-clickHandler = (e) => {
-    console.log(e.currentTarget.dataset.id )
-}
+    addTo = (e) => {
+        let newArray = this.state.passcode;
+        newArray.push(e.currentTarget.dataset.id)
+        this.setState( {passcode: newArray})
+        console.log(this.state.passcode)
+    };
 
-render() {
-    return (
-        <div id="pin">
-            <div className="dots">
-                <div className="dot dot1"></div>
-                <div className="dot dot2"></div>
-                <div className="dot dot3"></div>
-                <div className="dot dot4"></div>
+    render() {
+        return (
+            <div id="pin">
+                <div className="dots">
+                    <div className="dot dot1"></div>
+                    <div className="dot dot2"></div>
+                    <div className="dot dot3"></div>
+                    <div className="dot dot4"></div>
+                </div>
+                <br></br>
+                <p>Enter Your Pin</p>
+                <Numpad click={this.addTo} />
             </div>
-            <br></br>
-            <p>Enter Your Pin</p>
-            <Numpad click={this.clickHandler}/>
-        </div>
-    )
-}
+        )
+    }
 }
 
 export default Pin;
